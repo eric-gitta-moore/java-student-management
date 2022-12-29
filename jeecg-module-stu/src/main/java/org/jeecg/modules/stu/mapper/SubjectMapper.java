89c33e5df8999b96d52221300805aa21b2132426
Eric Moore <EricGittaMoore@duck.com>
2022-12-29 15:02:14 +0800

    feat: 科目管理

---
+++ jeecg-module-stu/src/main/java/org/jeecg/modules/stu/mapper/SubjectMapper.java
@@ -0,0 +1,17 @@
+package org.jeecg.modules.stu.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.jeecg.modules.stu.entity.Subject;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: 科目管理
+ * @Author: jeecg-boot
+ * @Date:   2022-12-29
+ * @Version: V1.0
+ */
+public interface SubjectMapper extends BaseMapper<Subject> {
+
+}
