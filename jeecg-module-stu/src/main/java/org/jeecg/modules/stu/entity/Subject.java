d48e07da14a00d48e52943ac71082bd79b9cc286
JamesCurtis <jamescurtis@foxmail.com>
2022-12-29 22:16:46 +0800

    style: 整理代码

--- jeecg-module-stu/src/main/java/org/jeecg/modules/stu/entity/Subject.java
+++ jeecg-module-stu/src/main/java/org/jeecg/modules/stu/entity/Subject.java
@@ -1,56 +1,67 @@
 package org.jeecg.modules.stu.entity;
 
-import java.io.Serializable;
-import java.io.UnsupportedEncodingException;
-import java.util.Date;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.TableLogic;
-import lombok.Data;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.jeecgframework.poi.excel.annotation.Excel;
-import org.jeecg.common.aspect.annotation.Dict;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
 
 /**
  * @Description: 科目管理
  * @Author: jeecg-boot
- * @Date:   2022-12-29
+ * @Date: 2022-12-29
  * @Version: V1.0
  */
-@ApiModel(value="stu_subject对象", description="科目管理")
+@ApiModel(value = "stu_subject对象", description = "科目管理")
 @Data
 @TableName("stu_subject")
 public class Subject implements Serializable {
+
     private static final long serialVersionUID = 1L;
 
-	/**主键*/
-	@TableId(type = IdType.ASSIGN_ID)
+    /**
+     * 主键
+     */
+    @TableId(type = IdType.ASSIGN_ID)
     @ApiModelProperty(value = "科目编号")
     private java.lang.String id;
-	/**科目名称*/
+    /**
+     * 科目名称
+     */
     @ApiModelProperty(value = "科目名称")
     private java.lang.String name;
-	/**创建人*/
+    /**
+     * 创建人
+     */
     @ApiModelProperty(value = "创建人")
     private java.lang.String createBy;
-	/**创建日期*/
-	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
-    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    /**
+     * 创建日期
+     */
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @ApiModelProperty(value = "创建日期")
     private java.util.Date createTime;
-	/**更新人*/
+    /**
+     * 更新人
+     */
     @ApiModelProperty(value = "更新人")
     private java.lang.String updateBy;
-	/**更新日期*/
-	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
-    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    /**
+     * 更新日期
+     */
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @ApiModelProperty(value = "更新日期")
     private java.util.Date updateTime;
-	/**所属部门*/
+    /**
+     * 所属部门
+     */
     @ApiModelProperty(value = "所属部门")
     private java.lang.String sysOrgCode;
 }
