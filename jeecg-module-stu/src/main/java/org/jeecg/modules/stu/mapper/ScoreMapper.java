f768d6208735a86f848d68f0c6e3ecc832efe5d3
Eric Moore <EricGittaMoore@duck.com>
2022-12-31 01:12:56 +0800

    feat(modules/stu): 学生成绩管理

---
+++ jeecg-module-stu/src/main/java/org/jeecg/modules/stu/mapper/ScoreMapper.java
@@ -0,0 +1,17 @@
+package org.jeecg.modules.stu.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.jeecg.modules.stu.entity.Score;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: 学生成绩
+ * @Author: jeecg-boot
+ * @Date:   2022-12-29
+ * @Version: V1.0
+ */
+public interface ScoreMapper extends BaseMapper<Score> {
+
+}
