0998d20eedb2824a074a1eb7ea652b90a4998d96
JamesCurtis <jamescurtis@foxmail.com>
2023-01-02 14:56:43 +0800

    chore(jeecg-module-biz/modules/test): 测试单表

---
+++ jeecg-module-stu/src/main/java/org/jeecg/modules/test/service/IAiControlSingleService.java
@@ -0,0 +1,14 @@
+package org.jeecg.modules.test.service;
+
+import org.jeecg.modules.test.entity.AiControlSingle;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Description: 表单控件@单表
+ * @Author: jeecg-boot
+ * @Date:   2023-01-02
+ * @Version: V1.0
+ */
+public interface IAiControlSingleService extends IService<AiControlSingle> {
+
+}
