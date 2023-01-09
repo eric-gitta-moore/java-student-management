1aac48a153ef767738c88416327907b8c1ac1ca7
Eric Moore <EricGittaMoore@duck.com>
2023-01-09 14:44:40 +0800

    fix(custom-modules-custom): 单体和微服务定义切换接口

---
+++ custom-modules-custom/custom-system-biz/src/main/java/com/example/modules/stu/service/impl/StuApiImpl.java
@@ -0,0 +1,16 @@
+package com.example.modules.stu.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.stu.api.IStuApi;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author w
+ */
+@Slf4j
+@Service
+public class StuApiImpl implements IStuApi {
+
+}
